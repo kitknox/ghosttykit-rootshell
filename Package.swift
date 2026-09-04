0697a2127d9f743b3eb211029dca9fc240b59c96
Kit Knox <kit@rootshell.com>
2026-09-03 17:01:46 -0700

    Publish GhosttyKit 0.2.6

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GhosttyKitAppStore",
-            url: "https://github.com/kitknox/ghosttykit-rootshell/releases/download/v0.2.5/GhosttyKitAppStore.xcframework.zip",
-            checksum: "d40657ac5a4a38aa19c574967700c96e53a32d1c5246e64127f1569e66acd7bd"
+            url: "https://github.com/kitknox/ghosttykit-rootshell/releases/download/v0.2.6/GhosttyKitAppStore.xcframework.zip",
+            checksum: "65d241c3358b89a7cef9929fe8d878cf5e80c1603142f874b9b916963bae2e89"
         ),
         .binaryTarget(
             name: "GhosttyKitStandalone",
-            url: "https://github.com/kitknox/ghosttykit-rootshell/releases/download/v0.2.5/GhosttyKitStandalone.xcframework.zip",
-            checksum: "2238dfe003d5ad28fca5337c66494c636689df450186b34e97608f6325df59f5"
+            url: "https://github.com/kitknox/ghosttykit-rootshell/releases/download/v0.2.6/GhosttyKitStandalone.xcframework.zip",
+            checksum: "e79ecc7afcfc8b9576dbf6f1597993953db40e885cdea71308314bd8a6f96669"
         ),
     ]
 )
